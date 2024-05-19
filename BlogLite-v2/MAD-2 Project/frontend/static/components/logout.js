778bc041981bd4f3b0766eaadc0c7cc1045f1123
Ranjith J S <76442505+Ranjith-JS2803@users.noreply.github.com>
2024-05-19 16:59:23 +0530

    Delete BlogLite-v2/MAD-2 Project directory

--- BlogLite-v2/MAD-2 Project/frontend/static/components/logout.js
+++
@@ -1,8 +0,0 @@
-export default{
-    template :`<div></div>`,
-    mounted(){
-        localStorage.removeItem("auth-token");
-        localStorage.removeItem("name");
-        this.$router.push('/')
-    }
-}
